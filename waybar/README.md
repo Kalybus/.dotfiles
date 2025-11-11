5301533be06efffc1ecc8548b43d5b7da96c4284
Kalybus <contact@jeromecoste.fr>
2025-11-11 17:25:08 +0100

    new waybar config

--- waybar/README.md
+++
@@ -1,9 +0,0 @@
-# Waybar configuration
-
-## Theme
-https://github.com/catppuccin/waybar
-
-## Reload config
-```bash
-$ pkill waybar && hyprctl dispatch exec waybar
-```
--- waybar/sync_to_pc.sh
+++ waybar/sync_to_pc.sh
@@ -3,8 +3,9 @@
 mkdir -p ~/.config/waybar
 
 # https://github.com/catppuccin/waybar
-cp ./mocha.css ~/.config/waybar/theme.css
 cp ./style.css ~/.config/waybar/style.css
 cp ./config.jsonc ~/.config/waybar/config.jsonc
 
 pkill waybar && hyprctl dispatch exec waybar
+#pkill waybar
+#env GTK_DEBUG=interactive waybar
